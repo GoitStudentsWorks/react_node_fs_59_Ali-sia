e532986d45b123d7849f11f36deed26c6e5b1d29
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-04-21 19:47:30 +0300

    add initial App structure

---
+++ src/components/App.jsx
@@ -0,0 +1,73 @@
+import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom';
+import { Layout } from './Layout';
+
+export const App = () => {
+  return (
+    <BrowserRouter basename="goose-track-team-4">
+      {/* <Suspense fallback={null}> */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* routes for authorization */}
+          <Route
+            path="/register"
+            element={
+              <h2>register</h2>
+              // <RestrictedRoute
+              //   redirectTo="/contacts"
+              //   component={<RegisterPage />}
+              // />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <h2>login</h2>
+              // <RestrictedRoute
+              //   redirectTo="/contacts"
+              //   component={<LoginPage />}
+              // />
+            }
+          />
+        </Route>
+        <Route>
+          {/* path="/" element={<h2>main layout</h2>} */}
+          <Route index element={<h2>main layout</h2>} />
+          <Route
+            path="/account"
+            element={
+              <h2>account</h2>
+              // <PrivateRoute redirectTo="/login" component={<AccountPage />} />
+            }
+          />
+          <Route
+            path="/calendar"
+            element={
+              <h2>calendar</h2>
+              // <PrivateRoute redirectTo="/login" component={<AccountPage />} />
+            }
+          >
+            <Route
+              index
+              path="/month/:currentDate"
+              element={
+                <h2>calendar month</h2>
+                // <PrivateRoute redirectTo="/login" component={<AccountPage />} />
+              }
+            />
+            <Route
+              index
+              path="/day/:currentDate"
+              element={
+                <h2>calendar day</h2>
+                // <PrivateRoute redirectTo="/login" component={<AccountPage />} />
+              }
+            />
+          </Route>
+        </Route>
+
+        <Route path="*" element={<h1>not found page</h1>} />
+      </Routes>
+      {/* </Suspense> */}
+    </BrowserRouter>
+  );
+};
