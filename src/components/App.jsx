e77744df9841c594a0acbc4ee4d60e982f03a94a
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-04-24 20:53:37 +0300

    first version of taking current user

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom';
 import { InitialElement } from './InitialElement';
-import { useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import { selectTheme } from 'redux/auth/auth.selectors';
 import { ThemeProvider } from 'styled-components';
 import { theme, light, dark } from 'theme';
@@ -13,57 +13,57 @@ import LoginPage from '../pages/LoginPage/LoginPage';
 
 export const App = () => {
   const currentTheme = useSelector(selectTheme);
-  const themeGlobal = { ...theme, colors: {light, dark}[currentTheme] };
+  const themeGlobal = { ...theme, colors: { light, dark }[currentTheme] };
 
   return (
     <ThemeProvider theme={themeGlobal}>
-    <BrowserRouter basename="goose-track-team-4">
-      {/* <Suspense fallback={null}> */}
-      <Routes>
-        <Route path="/" element={<InitialElement />}>
-          {/* routes for authorization */}
-          <Route
-            path="/register"
-            element={
-              <RegisterPage />
-              // <h2>register</h2>
-              // <RestrictedRoute
-              //   redirectTo="/contacts"
-              //   component={<RegisterPage />}
-              // />
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <LoginPage />
-              // <h2>login</h2>
-              // <RestrictedRoute
-              //   redirectTo="/contacts"
-              //   component={<LoginPage />}
-              // />
-            }
-          />
+      <BrowserRouter basename="goose-track-team-4">
+        {/* <Suspense fallback={null}> */}
+        <Routes>
+          <Route path="/" element={<InitialElement />}>
+            {/* routes for authorization */}
+            <Route
+              path="/register"
+              element={
+                <RegisterPage />
+                // <h2>register</h2>
+                // <RestrictedRoute
+                //   redirectTo="/contacts"
+                //   component={<RegisterPage />}
+                // />
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <LoginPage />
+                // <h2>login</h2>
+                // <RestrictedRoute
+                //   redirectTo="/contacts"
+                //   component={<LoginPage />}
+                // />
+              }
+            />
 
-          <Route element={<MainLayout />}>
-            <Route
-              path="/account"
-              element={
-                <AccountPage />
-                // <PrivateRoute redirectTo="/login" component={<AccountPage />} />
-              }
-            />
-            <Route
-              path="/calendar"
-              element={
-                <Navigate to={`/calendar/month/${Date.now()}`} replace />
-              }
-            />
-            <Route
-              path="/calendar/month/:currentDay"
-              element={<CalendarPage />}
-            />
-            {/* <Route
+            <Route element={<MainLayout />}>
+              <Route
+                path="/account"
+                element={
+                  <AccountPage />
+                  // <PrivateRoute redirectTo="/login" component={<AccountPage />} />
+                }
+              />
+              <Route
+                path="/calendar"
+                element={
+                  <Navigate to={`/calendar/month/${Date.now()}`} replace />
+                }
+              />
+              <Route
+                path="/calendar/month/:currentDay"
+                element={<CalendarPage />}
+              />
+              {/* <Route
               index
               path="/month/:currentDate"
               element={
@@ -79,13 +79,13 @@ export const App = () => {
                 // <PrivateRoute redirectTo="/login" component={<AccountPage />} />
               }
             />*/}
+            </Route>
           </Route>
-        </Route>
 
-        <Route path="*" element={<h1>not found page</h1>} />
-      </Routes>
-      {/* </Suspense> */}
-    </BrowserRouter>
+          <Route path="*" element={<h1>not found page</h1>} />
+        </Routes>
+        {/* </Suspense> */}
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
--- src/redux/auth/auth.operations.js
+++ src/redux/auth/auth.operations.js
@@ -1,18 +1,12 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-// import {
-//   clearAuthHeader,
-//   setAuthHeader,
-//   publicApi,
-//   privateApi,
-// } from 'http/http';
+// clearAuthHeader,
+// publicApi,
+
+import { setAuthHeader, privateApi } from 'services/http';
 
 axios.defaults.baseURL = 'https://goose-track-backend-8txo.onrender.com';
-
-const setAuthHeader = token => {
-  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-};
 
 /*
  * POST @ /users/signup
@@ -74,24 +68,22 @@ export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
 export const refreshUser = createAsyncThunk(
   'auth/refresh',
   async (_, thunkAPI) => {
-    // // Reading the token from the state via getState()
-    // const state = thunkAPI.getState();
-    // const persistedToken = state.auth.token;
+    // Reading the token from the state via getState()
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
 
-    // if (persistedToken === null) {
-    //   // If there is no token, exit without performing any request
-    //   return thunkAPI.rejectWithValue('Unable to fetch user');
-    // }
+    if (persistedToken === null) {
+      // If there is no token, exit without performing any request
+      return thunkAPI.rejectWithValue('Unable to fetch user');
+    }
 
-    // try {
-    return 0;
-
-    // // If there is a token, add it to the HTTP header and perform the request
-    // setAuthHeader(persistedToken);
-    // const res = await privateApi.get('/users/current');
-    // return res.data;
-    // } catch (error) {
-    //   return thunkAPI.rejectWithValue(error.message);
-    // }
+    try {
+      // If there is a token, add it to the HTTP header and perform the request
+      setAuthHeader(persistedToken);
+      const res = await privateApi.get('/users/current');
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
 );
