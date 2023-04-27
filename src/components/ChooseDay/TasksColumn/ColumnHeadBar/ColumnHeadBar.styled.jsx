4ce83a93f75f6140f1abc694bfd6cdc3ff0100c9
Natalya Karpova <knataly0513@gmail.com>
2023-04-27 17:23:12 +0300

    Update ColumnHeadBar.styled.jsx

---
+++ src/components/ChooseDay/TasksColumn/ColumnHeadBar/ColumnHeadBar.styled.jsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import { greaterThan } from 'helpers/breakpoints.styled';
+// import { Link } from 'react-router-dom';
+
+export const ColumnHeadBarWrapper = styled.div`
+  display: flex;
+  align-items: center;
+justify-content: space-between;
+
+
+  /* margin-bottom: ${p => p.theme.space[4]}px;
+
+  min-height: 174px;
+  width: 336px;
+
+  border-radius: ${p => p.theme.radii.light};
+  border: ${p => `${p.theme.borders.normal} ${p.theme.colors.taskBorderTheme}`};
+
+  font-size: ${p => p.theme.fontSizes.m};
+  font-weight: ${p => p.theme.fontWeights.semibold};
+  line-height: 1.12;
+
+  background-color: ${p => p.theme.colors.bgcSecondaryTheme};
+  color: ${p => p.theme.colors.textDaysNameAtDaysPage}; */
+
+  /* ${greaterThan(
+    'tablet',
+    `
+   min-height: 68px;
+   max-width: 704px;
+   font-size: 14px;
+`
+  )}
+  ${greaterThan(
+    'laptop',
+    `
+  max-width: 1087px;
+`
+  )} */
+// `;
+
