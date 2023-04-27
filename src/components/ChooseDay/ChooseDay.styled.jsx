c55881454243d54427b1b9b4e2613f5a734e885f
Natalya Karpova <knataly0513@gmail.com>
2023-04-27 17:27:24 +0300

    Update ChooseDay.styled.jsx

--- src/components/ChooseDay/ChooseDay.styled.jsx
+++ src/components/ChooseDay/ChooseDay.styled.jsx
@@ -1,43 +1,19 @@
 import styled from 'styled-components';
 import { greaterThan } from 'helpers/breakpoints.styled';
-// import { Link } from 'react-router-dom';
+
 export const ChoosedDayWrapper = styled.div`
-/* overflow-x: scroll; */
-/* width: 336px; */
 overflow: hidden;
 
 `;
 export const TasksColumnsListWrapper = styled.div`
-  /* display: flex; */
-  /* align-items: center; */
-  /* margin-bottom: ${p => p.theme.space[4]}px; */
-  /* flex-wrap: nowrap; */
   overflow-x: scroll;
   width: 336px;
-  /* overflow: hidden; */
-  /* width: fit-content; */
 `;
 export const TasksColumnsList = styled.div`
   display: flex;
-  /* align-items: center; */
   gap: 22px;
   margin-bottom: ${p => p.theme.space[4]}px;
-  /* flex-wrap: nowrap; */
-  /* overflow-x: scroll; */
   width: fit-content;
-
-  /* min-height: 74px;
-  max-width: 336px;
-
-  border-radius: ${p => p.theme.radii.light};
-  border: ${p => `${p.theme.borders.normal} ${p.theme.colors.taskBorderTheme}`};
-
-  font-size: ${p => p.theme.fontSizes.m};
-  font-weight: ${p => p.theme.fontWeights.semibold};
-  line-height: 1.12;
-
-  background-color: ${p => p.theme.colors.bgcSecondaryTheme};
-  color: ${p => p.theme.colors.textDaysNameAtDaysPage}; */
 
   /* ${greaterThan(
     'tablet',
