3f601518370e3d13baef2955f37a52f1c4432201
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-04-28 16:34:05 +0300

    auth nav for homepage

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import { AuthNav } from 'components/AuthNavigate/AuthNavigate';
+
+export const HomePage = () => {
+  return (
+    <div>
+      <AuthNav />
+      <h1>home</h1>
+    </div>
+  );
+};
