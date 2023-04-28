c0d4f7eca538ef27795eb608209d55f6326542c0
Natalya Karpova <knataly0513@gmail.com>
2023-04-28 16:47:20 +0300

    Update TasksColumn.styled.jsx

--- src/components/ChooseDay/TasksColumn/TasksColumn.styled.jsx
+++ src/components/ChooseDay/TasksColumn/TasksColumn.styled.jsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import { greaterThan } from 'helpers/breakpoints.styled';
-// import { Link } from 'react-router-dom';
 
 export const TaskColumnsWrapper = styled.div`
   display: flex;
