f36cd4f85b6d66a991894eb4e1c385c32f3630b9
Natalya Karpova <knataly0513@gmail.com>
2023-04-28 17:07:54 +0300

    Update ColumnHeadBar.styled.jsx

--- src/components/ChooseDay/TasksColumn/ColumnHeadBar/ColumnHeadBar.styled.jsx
+++ src/components/ChooseDay/TasksColumn/ColumnHeadBar/ColumnHeadBar.styled.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 // import { greaterThan } from 'helpers/breakpoints.styled';
 // import { Link } from 'react-router-dom';
-
+import { ReactComponent as RoundBtnIcon } from './RoundBtnIcon.svg';
 
 
 export const ColumnHeadBarWrapper = styled.div`
@@ -16,6 +16,22 @@ export const ColumnHeadBarTitle = styled.h2`
 `;
 
 export const StyledRoundButton = styled.button`
-    border: none;
-    background-color: inherit;
+    border: 0;
+    padding: 1px;
+    background-color: transparent;
+    cursor: pointer;
+    display: block;
+    &:hover,
+    &:focus {
+        box-shadow: ${p => p.theme.shadows.loginBtn};
+        border-radius: 50%;
+    };
+`;
+
+export const RoundBtnIconSvg = styled(RoundBtnIcon)`
+  display: block;
+  width: 22px;
+  height: 22px;
+  color: ${p => p.theme.colors.textHeaderTheme};
+
 `;
