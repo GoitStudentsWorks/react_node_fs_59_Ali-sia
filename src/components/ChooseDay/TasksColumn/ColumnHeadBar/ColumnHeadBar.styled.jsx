0ffdfc66ab4f3092b35d195d96b10daf3c112252
Natalya Karpova <knataly0513@gmail.com>
2023-04-28 16:47:16 +0300

    Update ColumnHeadBar.styled.jsx

--- src/components/ChooseDay/TasksColumn/ColumnHeadBar/ColumnHeadBar.styled.jsx
+++ src/components/ChooseDay/TasksColumn/ColumnHeadBar/ColumnHeadBar.styled.jsx
@@ -1,41 +1,21 @@
 import styled from 'styled-components';
-import { greaterThan } from 'helpers/breakpoints.styled';
+// import { greaterThan } from 'helpers/breakpoints.styled';
 // import { Link } from 'react-router-dom';
+
+
 
 export const ColumnHeadBarWrapper = styled.div`
   display: flex;
   align-items: center;
-justify-content: space-between;
+  justify-content: space-between;
 
+`;
+ 
+export const ColumnHeadBarTitle = styled.h2`
+  color: ${p => p.theme.colors.textHeaderTheme};
+`;
 
-  /* margin-bottom: ${p => p.theme.space[4]}px;
-
-  min-height: 174px;
-  width: 336px;
-
-  border-radius: ${p => p.theme.radii.light};
-  border: ${p => `${p.theme.borders.normal} ${p.theme.colors.taskBorderTheme}`};
-
-  font-size: ${p => p.theme.fontSizes.m};
-  font-weight: ${p => p.theme.fontWeights.semibold};
-  line-height: 1.12;
-
-  background-color: ${p => p.theme.colors.bgcSecondaryTheme};
-  color: ${p => p.theme.colors.textDaysNameAtDaysPage}; */
-
-  /* ${greaterThan(
-    'tablet',
-    `
-   min-height: 68px;
-   max-width: 704px;
-   font-size: 14px;
-`
-  )}
-  ${greaterThan(
-    'laptop',
-    `
-  max-width: 1087px;
-`
-  )} */
-// `;
-
+export const StyledRoundButton = styled.button`
+    border: none;
+    background-color: inherit;
+`;
