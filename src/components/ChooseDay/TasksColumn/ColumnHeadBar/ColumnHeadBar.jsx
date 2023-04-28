b70ede7524d6abdd6641d16ccbfafc31f4b2d1d2
Natalya Karpova <knataly0513@gmail.com>
2023-04-28 16:47:12 +0300

    Update ColumnHeadBar.jsx

--- src/components/ChooseDay/TasksColumn/ColumnHeadBar/ColumnHeadBar.jsx
+++ src/components/ChooseDay/TasksColumn/ColumnHeadBar/ColumnHeadBar.jsx
@@ -1,17 +1,21 @@
 import { useOutletContext } from 'react-router-dom';
-import { ColumnHeadBarWrapper } from './ColumnHeadBar.styled';
+import { ColumnHeadBarWrapper, ColumnHeadBarTitle, StyledRoundButton } from './ColumnHeadBar.styled';
+import RoundBtnIcon from './RoundBtnIcon.svg';
 
 export default function ColumnHeadBar() {
     const { toggleModal } = useOutletContext();
     
-
     return (
         <ColumnHeadBarWrapper>
-            <h2>ToDo</h2>
-            <button type="button" onClick={() => toggleModal()}>+</button>
+            <ColumnHeadBarTitle>ToDo</ColumnHeadBarTitle>
+            <StyledRoundButton
+                type="button"
+                onClick={() => toggleModal()}>
+                <img src={RoundBtnIcon} alt="+"></img>
+            </StyledRoundButton>
         </ColumnHeadBarWrapper>
-    )
-}
+    );
+};
 
 // 1. Компонент отримує в пропсах заголовок колонки групи та ідентифікатор ступеню виконання задач в цій колонці (To do | In progress | Done)
 // 2. Компонент рендерить блок розмітки 
