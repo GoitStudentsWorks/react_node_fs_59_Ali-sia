ff27b2e3eec6259942b86b8a4ffc88f098907024
InnaShtondenko <inna.mycase@gmail.com>
2023-04-28 17:29:03 +0300

    Create FeatureList.jsx

---
+++ src/components/FeatureList/FeatureList.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function FeatureList() {
+  return (
+    <div>
+      <h2>Hidden Features</h2>
+      <ul>
+            <li>
+                  <h3>1.</h3>
+                  <p>Calendar</p>
+                  <p>view</p>    
+                  <p>"GooseTrack's Calendar view provides a comprehensive overview of your schedule, displaying all your tasks, events, and appointments in a visually appealing and intuitive layout."</p>
+            </li>
+            <li>
+                <h3>2.</h3>
+                <p>all in</p>   
+                <p>one</p>
+                <p>"GooseTrack is an all-in-one productivity tool that helps you stay on top of your tasks, events, and deadlines. Say goodbye to scattered to-do lists and hello to streamlined productivity with GooseTrack."</p>
+            </li>
+            <li>
+                <h3>3.</h3>
+                <p>SIDEBAR</p>   
+                <p>"GooseTrack offers easy access to your account settings, calendar, and filters. The "My Account" section allows you to manage your profile information and preferences, while the calendar provides a quick and convenient way to view your upcoming events and tasks."</p>  
+            </li>
+      </ul>
+    </div>
+  );
+}
+
+export default FeatureList;
