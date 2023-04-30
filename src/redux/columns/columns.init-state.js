876862341b97bfed61cfa9719f809b3d9af4bb95
Natalya Karpova <knataly0513@gmail.com>
2023-04-30 22:43:28 +0300

    Update columns.init-state.js

--- src/redux/columns/columns.init-state.js
+++ src/redux/columns/columns.init-state.js
@@ -1,7 +1,11 @@
 export const columnsInitState = {
     filter: '',
     columns: {
-        categories: [],
+        categories: [
+            { id: 'column1', title: 'To do', number: 1 },
+            { id: 'column2', title: 'In progress', number: 2 },
+            { id: 'column3', title: 'Done', number: 3 },
+        ],
         numbers: [],
         isLoading: false,
         error: null,
