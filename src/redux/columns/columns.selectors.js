20349f7058875813765627c7f888e41a15f0c5e3
Natalya Karpova <knataly0513@gmail.com>
2023-05-01 00:41:53 +0300

    Update columns.selectors.js

--- src/redux/columns/columns.selectors.js
+++ src/redux/columns/columns.selectors.js
@@ -1,5 +1,5 @@
-export const selectCategories = state => state.columns;
+export const selectColumns = state => state.columns.columns.columns;
 
-export const selectIsLoading = state => state.columns.isLoading;
+export const selectIsLoading = state => state.columns.columns.isLoading;
 
-export const selectError = state => state.columns.error;
+export const selectError = state => state.columns.columns.error;
