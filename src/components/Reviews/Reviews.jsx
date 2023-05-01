25140f186c1fbc515f4f595f527a79a6d9efa12a
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-02 00:45:10 +0300

    Update Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -2,21 +2,21 @@ import React, { useState } from 'react';
 import { Carousel } from './Caorusel';
 
 function Reviews() {
-  const toggleReviews = () => {
-    setShowComments(!showComments);
-  };
+  // const toggleReviews = () => {
+  //   setShowComments(!showComments);
+  // };
 
   return (
     <div>
       <h2>Reviews</h2>
-      <button onClick={toggleReviews}>Show/Hide Comments</button>
-      {showComments && (
+      {/* <button onClick={toggleReviews}>Show/Hide Comments</button> */}
+      {/* {showComments && (
         <div>
           <p>Comment 1</p>
           <p>Comment 2</p>
           <p>Comment 3</p>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
