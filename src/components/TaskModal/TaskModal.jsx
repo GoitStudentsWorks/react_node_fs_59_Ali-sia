1b363512c6817e3bfd98336b13201291335befef
OleksandrR61 <106624948+OleksandrR61@users.noreply.github.com>
2023-05-01 11:50:16 +0300

    Update TaskModal.jsx

--- src/components/TaskModal/TaskModal.jsx
+++ src/components/TaskModal/TaskModal.jsx
@@ -14,6 +14,7 @@ function TaskModal({ task, category, onClose, isModalOpen }) {
 
   const handleSubmit = newData => {
     if (!task) {
+      console.log(newData);
       // If there's no initial data, we're creating a new task
       dispatch(addTask({...newData, date: activeDate}))
         .unwrap()
