bb97d50da1ccdf965dc1348998fcee44a5332ca4
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-02 22:05:38 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,28 +9,28 @@ import authorImg from './authorImg.png';
 import authorImg2 from './authorImg2.png';
 import rateImg from './rate.png';
 
-const reviews = [
-  {
-    review:
-      'GooseTrack is impressive, the calendar view and filter options make it easy to stay organized and focused. Highly recommended.',
-    author: 'Olena Doe ',
-    authorAvatar: authorImg,
-    rate: rateImg,
-  },
-  {
-    review:
-      'GooseTrack is incredibly helpful, the sidebar with account management, calendar, and filter options make navigation seamless. Great for staying organized.',
-    author: 'Alexander Hubbard ',
-    authorAvatar: authorImg2,
-    rate: rateImg,
-  },
-  {
-    review: 'Швидка доставка та добре запакований товар. Дякую',
-    author: 'Петро',
-    authorAvatar: authorImg,
-    rate: rateImg,
-  },
-];
+// const reviews = [
+//   {
+//     review:
+//       'GooseTrack is impressive, the calendar view and filter options make it easy to stay organized and focused. Highly recommended.',
+//     author: 'Olena Doe ',
+//     authorAvatar: authorImg,
+//     rate: rateImg,
+//   },
+//   {
+//     review:
+//       'GooseTrack is incredibly helpful, the sidebar with account management, calendar, and filter options make navigation seamless. Great for staying organized.',
+//     author: 'Alexander Hubbard ',
+//     authorAvatar: authorImg2,
+//     rate: rateImg,
+//   },
+//   {
+//     review: 'Швидка доставка та добре запакований товар. Дякую',
+//     author: 'Петро',
+//     authorAvatar: authorImg,
+//     rate: rateImg,
+//   },
+// ];
 
 export const HomePage = () => {
   return (
@@ -39,7 +39,7 @@ export const HomePage = () => {
       <Container>
         <FeatureList />
 
-        <Reviews reviews={reviews} />
+        {/* <Reviews reviews={reviews} /> */}
       </Container>
     </div>
   );
