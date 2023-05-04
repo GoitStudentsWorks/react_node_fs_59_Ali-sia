cf1314444a853dd110e70537c73756dd903ad385
OleksandrR61 <106624948+OleksandrR61@users.noreply.github.com>
2023-05-04 22:36:46 +0300

    add modal
    Added modal to remove a column

--- src/components/ColumnToolbar/ColumnToolbar.jsx
+++ src/components/ColumnToolbar/ColumnToolbar.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectTheme } from 'redux/auth/auth.selectors';
 import { deleteColumn } from 'redux/columns/columns.operations';
 import { toast } from 'react-hot-toast';
 
+import Modal from 'components/Modal/Modal';
+
 import {
   Toolbar,
   Button,
   DeleteTaskIcon,
+  StyledP,
+  ButtonsWrapper,
+  StyledButton,
 } from './ColumnToolbar.styled';
 
 const ColumnToolbar = ({column, tasksForColumn}) => {
+  const [ isModalOpen, setIsModalOpen ] = useState(false)
+
   const dispatch = useDispatch();
   const currentTheme = useSelector(selectTheme);
   
   const handleDelete = id => {
+    setIsModalOpen(false);
     if (tasksForColumn.length !== 0) {
       toast.error(`Unable to delete column with tasks`);
       return;
@@ -27,11 +35,24 @@ const ColumnToolbar = ({column, tasksForColumn}) => {
   };
 
   return (
-    <Toolbar>
-      <Button onClick={() => handleDelete(column._id)}>
-        <DeleteTaskIcon theme={currentTheme} />
-      </Button>
-   </Toolbar>
+    <>
+      <Toolbar>
+        <Button onClick={() => setIsModalOpen(true)}>
+          <DeleteTaskIcon theme={currentTheme} />
+        </Button>
+      </Toolbar>
+      <Modal onClose={() => setIsModalOpen(false)} isModalOpen={isModalOpen}>
+        <StyledP>Are you sure you want to log out?</StyledP>
+        <ButtonsWrapper>
+          <StyledButton type="button" onClick={() => handleDelete(column._id)}>
+            Yes
+          </StyledButton>
+          <StyledButton type="button" onClick={() => setIsModalOpen(false)}>
+            No
+          </StyledButton>
+        </ButtonsWrapper>
+      </Modal>
+    </>
   );
 };
 
